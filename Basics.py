d174e24be69c94373ffbf793db12504fd15a1375
Saber <saberkhoshdel@users.noreply.github.com>
2020-10-15 00:07:14 -0400

    Initial commit

---
+++ Basics.py
@@ -0,0 +1,47 @@
+
+"""
+Created on Thu Sep 26 22:31:25 2019
+@author: snikkho based on Mosh Hamedani courses on Youtube
+"""
+#Basics
+# Begin and end comments in paragraphs with """
+
+# first, print a string
+# print("Hello World\nThis is Saber")
+
+"""left off
+Started on 10/13/2020 with 
+Coursera Python for Data Science and AI
+""" 
+
+""" Week 1
+Types: integer (int), float, string (str), and boolean (bool)
+bool output is True or False
+type(True):bool
+int(True):1
+int(False):0
+bool(0):False
+bool(1):True
+
+Expressions: + - / *
+default "/" is a float type
+integer division result: //
+11//2: 5
+Python complies with math order of operations 
+
+Variables
+x= int(2)
+x=2/1
+type(x): float
+"""
+
+#Lab
+# Try your first Python output
+print('Hello, Python!')
+
+# Check the Python Version
+import sys
+print(sys.version)
+
+# System settings about float type
+sys.float_info
